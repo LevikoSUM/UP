c7a4600f3546ae1a28ef42efb81c06481c4a4480
LevikoSUM <115878070+LevikoSUM@users.noreply.github.com>
2024-11-11 22:53:20 +0200

    Create Task2.cpp

---
+++ Sem.05/Pract.05/Kosta/Task2.cpp
@@ -0,0 +1,17 @@
+int minMaxValues(int arr[], int size, int& max)
+{
+	int min = arr[0];
+	max = arr[0];
+	for (int i = 1; i < size; i++)
+	{
+		if (arr[i] > max)
+		{
+			max = arr[i];
+		}
+		if (arr[i] < min)
+		{
+			min = arr[i];
+		}
+	}
+	return min;
+}
